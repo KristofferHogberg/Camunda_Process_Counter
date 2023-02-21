5d2c4b61c0705afad6e65b0cb951a6f257a6bc30
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2023-02-21 15:36:07 +0100

    added basic endpoints

--- Apendo_Count_Processes_UI/scripts/script.js
+++ Apendo_Count_Processes_UI/scripts/script.js
@@ -1,4 +1,4 @@
-async function getAllProcesses() {
+async function getProcessById() {
   const processId = document.getElementById('processIdGet').value;
 
   if (!processId) {
@@ -13,8 +13,56 @@ async function getAllProcesses() {
   const response = await fetch(endpoint);
 
   const data = await response.json();
-  console.log(data);
+
+  // const dataDisplayElement = document.getElementById('dataDisplay');
+  // dataDisplayElement.innerHTML = JSON.stringify(data.null, 2);
+  const dataDisplayElement = document.getElementById('dataDisplay');
+  dataDisplayElement.innerHTML =
+    '<pre><code class="json">' +
+    JSON.stringify(data, null, 2) +
+    '</code></pre>';
+  hljs.highlightBlock(dataDisplayElement);
+}
+
+async function getAllProcesses() {
+  const processId = document.getElementById('processIdGet').value;
+
+  // if (!processId) {
+  //   alert('Please input a process id.');
+  //   return;
+  // }
+
+  const endpoint = new URL(
+    `http://localhost:8080/engine-rest/history/process-instance`
+  );
+  endpoint.searchParams.set('token', 'MY_TOKEN_HERE');
+  const response = await fetch(endpoint);
+
+  const data = await response.json();
 
   const dataDisplayElement = document.getElementById('dataDisplay');
-  dataDisplayElement.innerHTML = JSON.stringify(data);
+  dataDisplayElement.innerHTML =
+    '<pre><code class="json">' +
+    JSON.stringify(data, null, 2) +
+    '</code></pre>';
+  hljs.highlightBlock(dataDisplayElement);
+}
+
+async function getProcessCount() {
+  const processId = document.getElementById('processIdGet').value;
+
+  const endpoint = new URL(
+    `http://localhost:8080/engine-rest/history/process-instance`
+  );
+  endpoint.searchParams.set('token', 'MY_TOKEN_HERE');
+  const response = await fetch(endpoint);
+
+  const data = await response.json();
+
+  const dataDisplayElement = document.getElementById('dataDisplay');
+  dataDisplayElement.innerHTML =
+    '<pre><code class="json">' +
+    JSON.stringify(data, null, 2) +
+    '</code></pre>';
+  hljs.highlightBlock(dataDisplayElement);
 }
