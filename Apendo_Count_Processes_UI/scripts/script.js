0c7863cdf0c00dec8972593a6861ef38aa567791
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2023-02-22 10:23:20 +0100

    added input dates to form and basic css. Added scripts, getAll, getById, and getCount to porcesses endpounts

--- Apendo_Count_Processes_UI/scripts/script.js
+++ Apendo_Count_Processes_UI/scripts/script.js
@@ -13,33 +13,6 @@ async function getProcessById() {
   const response = await fetch(endpoint);
 
   const data = await response.json();
-
-  // const dataDisplayElement = document.getElementById('dataDisplay');
-  // dataDisplayElement.innerHTML = JSON.stringify(data.null, 2);
-  const dataDisplayElement = document.getElementById('dataDisplay');
-  dataDisplayElement.innerHTML =
-    '<pre><code class="json">' +
-    JSON.stringify(data, null, 2) +
-    '</code></pre>';
-  hljs.highlightBlock(dataDisplayElement);
-}
-
-async function getAllProcesses() {
-  const processId = document.getElementById('processIdGet').value;
-
-  // if (!processId) {
-  //   alert('Please input a process id.');
-  //   return;
-  // }
-
-  const endpoint = new URL(
-    `http://localhost:8080/engine-rest/history/process-instance`
-  );
-  endpoint.searchParams.set('token', 'MY_TOKEN_HERE');
-  const response = await fetch(endpoint);
-
-  const data = await response.json();
-
   const dataDisplayElement = document.getElementById('dataDisplay');
   dataDisplayElement.innerHTML =
     '<pre><code class="json">' +
@@ -65,4 +38,29 @@ async function getProcessCount() {
     JSON.stringify(data, null, 2) +
     '</code></pre>';
   hljs.highlightBlock(dataDisplayElement);
+}
+
+async function getAllProcesses() {
+  console.log('Getting processes...');
+  const endpoint = new URL(
+    `http://localhost:8080/engine-rest/history/process-instance`
+  );
+  endpoint.searchParams.set('token', 'MY_TOKEN_HERE');
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  console.log('Got data:', data);
+
+  const dataDisplayElement = document.getElementById('dataDisplay');
+  dataDisplayElement.innerHTML =
+    '<pre><code class="json">' +
+    JSON.stringify(data, null, 2) +
+    '</code></pre>';
+  hljs.highlightBlock(dataDisplayElement);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log('Form submitted!');
+  getAllProcesses();
 }
