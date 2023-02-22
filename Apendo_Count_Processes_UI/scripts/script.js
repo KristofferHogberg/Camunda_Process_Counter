53745591a3e2983dfeea0ff4655afab469b7b797
Kristoffer Högberg <kristoffer.hogberg@apendo.se>
2023-02-22 14:03:34 +0100

    basic counter is working, count all and count process by process id

--- Apendo_Count_Processes_UI/scripts/script.js
+++ Apendo_Count_Processes_UI/scripts/script.js
@@ -1,71 +1,68 @@
-async function getProcessById() {
+async function getProcessCount() {
   const processId = document.getElementById('processIdGet').value;
+  let endpoint;
 
-  // if (!processId) {
-  //   alert('Please input a process id.');
-  //   return;
-  // }
+  if (processId) {
+    const process = await getProcessById(processId);
 
+    console.log(process.length);
+    return { count: process.length };
+  } else {
+    endpoint = new URL(
+      // `http://localhost:8080/engine-rest/history/activity-instance/count`
+      `http://localhost:8080/engine-rest/history/process-instance/count`
+    );
+  }
+  // endpoint.searchParams.set('token', 'MY_TOKEN_HERE');
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  return data;
+}
+
+async function getProcessById(processId) {
   const endpoint = new URL(
     `http://localhost:8080/engine-rest/history/process-instance?processDefinitionKey=${processId}`
   );
-  endpoint.searchParams.set('token', 'MY_TOKEN_HERE');
+  // endpoint.searchParams.set('token', 'MY_TOKEN_HERE');
   const response = await fetch(endpoint);
 
-  const data = await response.json();
-  const dataDisplayElement = document.getElementById('dataDisplay');
-  dataDisplayElement.innerHTML =
-    '<pre><code class="json">' +
-    JSON.stringify(data, null, 2) +
-    '</code></pre>';
-  hljs.highlightBlock(dataDisplayElement);
-}
-
-async function getProcessCount() {
-  const processId = document.getElementById('processIdGet').value;
-
-  const endpoint = new URL(
-    `http://localhost:8080/engine-rest/history/process-instance`
-  );
-  endpoint.searchParams.set('token', 'MY_TOKEN_HERE');
-  const response = await fetch(endpoint);
-
-  const data = await response.json();
-
-  const dataDisplayElement = document.getElementById('dataDisplay');
-  dataDisplayElement.innerHTML =
-    '<pre><code class="json">' +
-    JSON.stringify(data, null, 2) +
-    '</code></pre>';
-  hljs.highlightBlock(dataDisplayElement);
+  return response.json();
 }
 
 async function getAllProcesses() {
-  console.log('Getting processes...');
   const endpoint = new URL(
     `http://localhost:8080/engine-rest/history/process-instance`
   );
-  endpoint.searchParams.set('token', 'MY_TOKEN_HERE');
+  // endpoint.searchParams.set('token', 'MY_TOKEN_HERE');
   const response = await fetch(endpoint);
-  const data = await response.json();
+  return await response.json();
+}
 
-  console.log('Got data:', data);
+async function handleSubmit(event) {
+  event.preventDefault();
+  const processId = document.getElementById('processIdGet').value;
+  let dataDisplayElement = document.getElementById('dataDisplay');
 
-  const dataDisplayElement = document.getElementById('dataDisplay');
+  let data;
+  let count;
+
+  if (processId) {
+    data = await getProcessById(processId);
+  } else {
+    data = await getAllProcesses();
+  }
+
+  count = await getProcessCount();
+  const countResponse = await getProcessCount();
+  count = countResponse.count;
+
+  dataDisplayElement = document.getElementById('dataDisplay');
   dataDisplayElement.innerHTML =
+    '<p>Process count: ' +
+    count +
+    '</p>' +
     '<pre><code class="json">' +
     JSON.stringify(data, null, 2) +
     '</code></pre>';
-  hljs.highlightBlock(dataDisplayElement);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  const processId = document.getElementById('processIdGet').value;
-
-  if (!processId) {
-    getAllProcesses();
-  } else {
-    getProcessById();
-  }
 }
